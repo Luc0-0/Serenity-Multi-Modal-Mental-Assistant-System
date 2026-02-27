7ee3c5cd6c56ac3780e111460a730178ffcab685
Nipun <nipunsujesh28@gmail.com>
2026-02-27 22:14:46 +0530

    switch to esbuild minifier

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-      mangle: {
-        toplevel: true, // Mangle top-level variable names
-        properties: false, // Too risky for React props
-      }
-    }
+    minify: 'esbuild',
   },
   server: {
     port: 5173,
